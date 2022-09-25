c84365d1232536b81668b57cca7a066ae2fc7bec
John <john.bow7720@gmail.com>
2022-09-24 19:40:19 -0600

    write errors to stderr instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn main() {
 
     // store our query and file args, ignoring the program's name in index 0
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = rusty_grep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
